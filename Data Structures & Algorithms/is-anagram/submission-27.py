9cd0f4ddc058c54dd3dd6b3f6a4d2ddf61598273
Syed Rayyan Ali <143465817+Ranoobaba@users.noreply.github.com>
2026-09-24 00:06:52 -0700

    Add: is-anagram - submission-27

---
+++ Data Structures & Algorithms/is-anagram/submission-27.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = sorted(S)
+        t = sorted(t)
+        if t == s:
+            return True
+        else:
+            return False
+        
+        
